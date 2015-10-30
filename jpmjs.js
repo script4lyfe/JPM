5f3e4032f37b5dc222b486a06208f597c3d6d5c6
script4lyfe <jessematarazzo@gmail.com>
2015-10-30 14:08:02 -0500

    Initial commit

---
+++ jpmjs.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Jesse on 9/23/2015.
+ */
+/*alert('hello');
+
+function print(message) {
+    var outputDiv = document.getElementById('poop');
+    outputDiv.innerHTML = message;
+}
+
+var poop = ["gross", "runny", "hard", "corny", "green",];
+var question = prompt("What type of poop do you like?")
+
+for(i = 0; i <= poop.length; i += 1){
+              if(question === "Corn" ){
+                  print(poop);
+
+              }
+
+}
+*/
+
